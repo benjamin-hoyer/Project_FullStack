324eead273480318f0886574ea2fe2702b10acc4
behoyer <benhoyer02@gmail.com>
2023-05-19 22:26:00 +0200

    Release 0.0: Project Structure and Initialisation

---
+++ src/server.js
@@ -0,0 +1,73 @@
+import Hapi from "@hapi/hapi";
+import Inert from "@hapi/inert";
+import Vision from "@hapi/vision";
+import Cookie from "@hapi/cookie";
+import Handlebars from "handlebars";
+
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+import { accountsController } from "./controllers/accounts-controller.js";
+import { webRoutes } from "./web-routes.js";
+import { db } from "./models/db.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Dotenv Configuration
+const result = dotenv.config();
+if (result.error) {
+  console.log(result.error.message);
+}
+
+async function init() {
+  const server = Hapi.server({
+    port: process.env.PORT || 4000,
+    routes: { cors: true },
+  });
+
+  await server.register(Inert);
+  await server.register(Vision);
+  await server.register(Cookie);
+
+  // Views Configuration
+  server.views({
+    engines: {
+      hbs: Handlebars,
+    },
+    relativeTo: __dirname,
+    path: "./pages",
+    layoutPath: "./pages",
+    partialsPath: "./pages/components",
+    layout: true,
+    isCached: false,
+  });
+
+  // Cookie Configuration
+  server.auth.strategy("session", "cookie", {
+    cookie: {
+      name: process.env.cookie_name,
+      password: process.env.cookie_password,
+      isSecure: false,
+    },
+    redirectTo: "/",
+    validate: accountsController.validate,
+  });
+  server.auth.default("session");
+
+  // Data bank
+  db.init("mongo");
+
+  // Routes
+  server.route(webRoutes);
+
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
+}
+
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+await init();
